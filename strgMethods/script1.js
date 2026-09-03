a72078f0b67806dada4e222f6d2f5af2779ddd9a
varunvarunsr05-cp <varunvarunsr05@gmail.com>
2026-09-03 19:21:16 +0530

    revised string methods

---
+++ strgMethods/script1.js
@@ -0,0 +1,40 @@
+let str="Hello World";
+let str1=[...str];//str1 collects characterWise with commans ="str H,e,l,l,o, ,W,o,r,l,dand"
+// let [...str1]=str;//same
+// str1+="and"
+// console.log("str",str);
+
+// //Lenght
+// let len=str.length;
+// console.log("len",len);
+
+// //concat()
+// let str2=" hi";
+// let conc=str+str2;
+// console.log(conc);
+
+// //indexOf()
+// let index=str.indexOf("d");
+// console.log("index is",index);
+
+//slice()
+// console.log(str.slice(6,10))
+// console.log(str);
+
+//replace
+// let rep=str.replace("World","Universe")
+// console.log(rep);
+
+//toUpperCase & toLowerCase()
+// let lower=str.toLocaleLowerCase();
+// let upper=str.toLocaleUpperCase();
+// console.log(upper);
+
+// //trim()
+// let srr="  i am bad  ";
+// console.log(srr.trim());
+// console.log(srr);
+
+//split()
+// let split=str.split("");
+// console.log(split);
