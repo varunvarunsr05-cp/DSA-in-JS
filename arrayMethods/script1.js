e903feae4e98d1f56bd50ff9bfb35a5b0b234de2
varunvarunsr05-cp <varunvarunsr05@gmail.com>
2026-09-05 08:31:23 +0530

    revised array and it's methods in js

---
+++ arrayMethods/script1.js
@@ -0,0 +1,36 @@
+let Arr=[1,"hello",[3,4]];
+
+//lenght
+// let len=Arr.length;
+// console.log("len",len);;
+// console.log(Arr);
+
+//push:adds element at last.
+// Arr.push("world");
+// console.log(Arr)
+
+//pop:removes last element.
+// Arr.pop();
+// console.log(Arr)
+
+//shift:removes first element.
+// console.log(Arr)
+// Arr.shift()
+// console.log(Arr)
+
+//unshift:adds element at first.
+// Arr.unshift("tom");
+// console.log(Arr)
+
+//slice:return a portion of arr based on index.
+// console.log(Arr)
+// console.log(Arr.slice(0,1))
+
+//Splice:removes and replaces an element inside the array.
+// console.log(Arr)
+// let slice=Arr.splice(2,3,"raaga");
+// console.log(Arr)
+// console.log(slice)
+
+
+
