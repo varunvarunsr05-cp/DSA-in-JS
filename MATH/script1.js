963e2b3424786687337dc7ace56a16541031e742
varunvarunsr05-cp <varunvarunsr05@gmail.com>
2026-09-05 08:28:15 +0530

    revised math functions

---
+++ MATH/script1.js
@@ -0,0 +1,17 @@
+//Math functions and methods
+console.log(Math.random())//random() for random nums
+
+//Floor-round-down to nearest intgr
+console.log(Math.floor(8.9));//8
+
+//Ciel() round-up nearest intgr
+console.log(Math.ceil(8.1));//9
+
+//Max & Min- to find respective value in a set
+console.log(Math.max(1,2,344,5,6,0))
+console.log(Math.min(1,2,344,5,6,0))
+
+//Pi- for value of pi.
+//E - for mathematical constant e.
+console.log(Math.PI)
+console.log(Math.E)
