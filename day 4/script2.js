129754a972ededa04728e6f44b281db9d6b6668a
varunvarunsr05-cp <varunvarunsr05@gmail.com>
2026-09-11 16:58:31 +0530

    day 4 qn2 revision

--- day 4/script2.js
+++ day 4/script2.js
@@ -24,18 +24,54 @@
 
 // longestWord(sentence)
 
-let sentence="I love lear JavaScript booooooooooooo";
+let sentence="I love lea JavaScript bo";
 
-function findLrgWord(sentence){
-    let words=sentence.split(" ");
-    let lrgWord=words[0];
-
-    for(let word of words){
-        if (word.length>lrgWord.length) {
-            lrgWord=word;
+function findLarge(sentence){
+    let sentences=sentence.split(" ");
+    let lrgWord=sentence[0];
+    // console.log(sentences)
+    sentences.forEach(e => {
+        // console.log(e);
+        if (e.length>lrgWord.length) {
+             lrgWord=e;
         }
-    }
+    });
     console.log(lrgWord);
 }
+findLarge(sentence);
+// function findLrgWord(sentence){
+//     let words=sentence.split(" ");
+//     let lrgWord=words[0];
 
-findLrgWord(sentence)
+//     for(let word of words){
+//         if (word.length>lrgWord.length) {
+//             lrgWord=word;
+//         }
+//     }
+//     console.log(lrgWord);
+// }
+
+// findLrgWord(sentence)
+
+//write a function to sort an array without using the inbuilt functions.
+
+// let Arr=[3,4,7,22,1,0,76,5,11]
+
+// function sortArr(Arr){
+//     let srtArr=[];
+//     // let min=Arr[0];
+
+//     for (let i = 0; i < Arr.length-1; i++) {
+//         for (let j = i+1; j < Arr.length; j++) {
+//             if (Arr[j]<Arr[i]) {
+//                 let temp=Arr[i];
+//                 Arr[i]=Arr[j]
+//                 Arr[j]=temp;
+//             }
+//         }
+//     }
+//     console.log(Arr);
+//     // console.log(srtArr);
+//     // return srtArr;
+// }
+// sortArr(Arr)
