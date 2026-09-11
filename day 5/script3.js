24b9396c5e2bcc76ce3cabb896f76fa5ce91abac
varunvarunsr05-cp <varunvarunsr05@gmail.com>
2026-09-11 17:00:04 +0530

    day 5 qn3 revision

--- day 5/script3.js
+++ day 5/script3.js
@@ -1,27 +1,44 @@
 //write a function to find the most frequently occuring number in an array
 
-let nums=[1,2,2,3,3,3,4,4,4,4];
+let nums=[1,2,2,6,6,6,66,6,6,3,3,3,4,4,5,5,5,5];
 
 function frequentNum(nums){
-    let count=0;
-    let obj={};
-    let freqNum;
-    let ferqNum1;
-
-    for (const num of nums) {
-        obj[num]=(obj[num] || 0)+1
-        if (obj[num]>count) {
-            count=obj[num];
-            freqNum=num;
-        }
+    let freq={};
+    let mFreq;
+    nums.forEach(e => {
+        freq[e]=(freq[e] || 0)+1;
+        // console.log(freq);
+    });
+    for(let key in freq){
+        let freqN=1;
+        if (freq[key]>freqN) {
+            freqN=freq[key]
+            mFreq=key;
+        }    
     }
-    for (const key in obj) {
-        if (obj[key]===count && key!==freqNum){
-            freqNum1=key;
-        }        
-    }
-    console.log(`fer1=${freqNum},fre2=${freqNum1}`)
+    console.log(mFreq);
 }
 
-// console.log(frequentNum(nums))
 frequentNum(nums)
+// function frequentNum(nums){
+//     let count=0;
+//     let obj={};
+//     let freqNum;
+//     let ferqNum1;
+
+//     for (const num of nums) {
+//         obj[num]=(obj[num] || 0)+1
+//         if (obj[num]>count) {
+//             count=obj[num];
+//             freqNum=num;
+//         }
+//     }
+//     for (const key in obj) {
+//         if (obj[key]===count && key!==freqNum){
+//             freqNum1=key;
+//         }        
+//     }
+//     console.log(`fer1=${freqNum},fre2=${freqNum1}`)
+// }
+
+// console.log(frequentNum(nums))
